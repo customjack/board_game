81930eec6edca3ada0ed09d1fcdf757f10ef62ea
customjack <customjack@gmail.com>
2025-11-28 21:17:14 -0500

    remove example plugin; replacing with submodule

--- plugins/example/machines/demo-phase-machine.js
+++
@@ -1,9 +0,0 @@
-export const createDemoPhaseStateMachine = (PhaseStateMachine, GamePhases, TurnPhases) =>
-    class DemoPhaseStateMachine extends PhaseStateMachine {
-        constructor(config = {}, eventBus = null) {
-            super({
-                gamePhases: config.gamePhases || Object.values(GamePhases),
-                turnPhases: config.turnPhases || Object.values(TurnPhases)
-            }, eventBus);
-        }
-    };
--- plugins/example/pieces/demo-piece-manager.js
+++
@@ -1,6 +0,0 @@
-export const createDemoPieceManager = (BasePieceManager) =>
-    class DemoPieceManager extends BasePieceManager {
-        buildRenderablePieces() {
-            return [];
-        }
-    };
