751d43071888d6abafb8bfd6aff69b302eae2d26
jaca230 <customjack@gmail.com>
2025-12-30 00:16:04 -0500

    Fix UIController helper import after engine move

--- src/js/game/engines/turn_based/components/UIController.js
+++ src/js/game/engines/turn_based/components/UIController.js
@@ -1,4 +1,4 @@
-import { getVisibleElementById } from '../../infrastructure/utils/helpers.js';
+import { getVisibleElementById } from '../../../infrastructure/utils/helpers.js';
 
 /**
  * UIController - Manages UI elements and their interactions
